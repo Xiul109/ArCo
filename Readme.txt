928c085a0bfecd165e127cd57371c74025307726
Luis Cabañero Gómez <xiul10901@gmail.com>
2015-10-20 16:37:07 +0200

    Subido Scrip para compilar y ejecutar con perf los programas de la Práctica 3  y guardar los datos de ésta en un fichero csv

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Esto es un scrip para compilar y ejecutar con perf los programas de la Práctica 3  y guardar los datos de ésta en un fichero csv, modificando el original proporcionado por los profesores.
---
+++ gen_result.py
@@ -0,0 +1,25 @@
+import sys, re, csv
+from subprocess import *
+
+kernel_number = (1, 2, 3 ,4, 5, 6, 7, 8, 9)
+
+tabla=[]
+tabla.append(["","Fallos en bifurcaciones","Bifurcaciones"])
+
+for i in kernel_number:
+    print("Compilando version {0}...".format(i))
+    if (call("gcc -O0 -o branch{0} branch{1}.c".format(i,i), shell=True)==-1):
+      print("Ha ocurrido un error durante la compilacion")
+      sys.exit(0)
+
+
+    print("Ejecutando version {0}...".format(i))
+    texto=Popen("sudo perf stat  -e instructions:u,branch-misses:u,branches:u ./branch{0}".format(i), shell=True,stderr=PIPE,stdout=PIPE).communicate()[1].split("\n")
+    tabla.append(["branch{0}".format(i),re.search("(?<= )\S+(?= )",texto[4]).group(0),re.search("(?<= )\S+(?= )",texto[5]).group(0)])
+
+print("Generando fichero...")
+with open("bifurcaciones.csv","wb") as f:
+    writer=csv.writer(f)
+    writer.writerows(tabla)
+
+print("Listo!")
